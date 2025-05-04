f754372d707b3c09e4b37191284a7a25cb8547d3
Ozgur Emin Nuhoglu <ozgureminnuhoglu@gmail.com>
2025-05-04 12:10:21 +0300

    20250504-LearnJava5

---
+++ java03/src/LearnJava5.java
@@ -0,0 +1,30 @@
+class Car {
+    String model;
+    int speed;
+
+    // Constructor
+    public Car(String carModel) {
+        this.model = carModel;
+        this.speed = 0;  // Start with speed 0
+    }
+
+    // Instance method to accelerate
+    public void accelerate(int increase) {
+        speed += increase;
+        System.out.println(model + " is now going at " + speed + " mph.");
+    }
+
+    // Instance method that calls accelerate
+    public void startDriving() {
+        System.out.println(model + " is starting to move!");
+        accelerate(50);  // Calls the accelerate method
+    }
+}
+
+public class LearnJava5 {
+    public static void main(String[] args) {
+        Car myCar = new Car("mercedes");
+        myCar.startDriving();  // Output: Toyota is starting to move!
+        //         Toyota is now going at 20 mph.
+    }
+}
